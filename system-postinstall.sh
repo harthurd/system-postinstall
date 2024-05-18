05797ee7dcc9629b38209150fa51d02ac3316a78
harthurd <55783388+harthurd@users.noreply.github.com>
2024-05-18 22:24:53 +0300

    Update system-postinstall.sh

--- system-postinstall.sh
+++ system-postinstall.sh
@@ -1,7 +1,10 @@
 # Install software
 dnf update -y
-rpm --import https://packages.microsoft.com/keys/microsoft.asc https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+rpm --import https://www.virtualbox.org/download/oracle_vbox_2016.asc
+rpm --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+wget -O /etc/yum.repos.d/virtualbox.repo https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo
 dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 dnf check-update
 dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
