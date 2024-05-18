37a9e0301c583aec4cb4a3e5c89343048f62c410
harthurd <55783388+harthurd@users.noreply.github.com>
2024-05-18 12:07:03 -0400

    Update system-postinstall.sh

--- system-postinstall.sh
+++ system-postinstall.sh
@@ -1,7 +1,9 @@
+# Install software
 dnf update -y
-mkdir -p /tmp/distrib
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -P /tmp/distrib && dnf install -y /tmp/distrib/google-chrome-stable_current_x86_64.rpm
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf check-update
-dnf install mc htop alacritty code
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm mc htop alacritty code obs-studio vlc
+flatpak install flathub md.obsidian.Obsidian com.skype.Client com.discordapp.Discord
