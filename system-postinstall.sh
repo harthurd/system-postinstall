e4e34db73c334eb3556d413838fd627883f73a7e
harthurd <55783388+harthurd@users.noreply.github.com>
2024-05-18 11:44:12 -0400

    Update system-postinstall.sh

--- system-postinstall.sh
+++ system-postinstall.sh
@@ -1,2 +1,7 @@
 dnf update -y
-dnf install mc htop alacritty 
+mkdir -p /tmp/distrib
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -P /tmp/distrib && dnf install -y /tmp/distrib/google-chrome-stable_current_x86_64.rpm
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+dnf check-update
+dnf install mc htop alacritty code
