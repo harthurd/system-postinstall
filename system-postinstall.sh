a6eac71ac68c2bad197fa756fa1b9cc81ba01827
harthurd <55783388+harthurd@users.noreply.github.com>
2024-05-18 12:36:49 -0400

    Update system-postinstall.sh

--- system-postinstall.sh
+++ system-postinstall.sh
@@ -1,7 +1,6 @@
 # Install software
 dnf update -y
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-rpm --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+rpm --import https://packages.microsoft.com/keys/microsoft.asc https://download.sublimetext.com/sublimehq-rpm-pub.gpg
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 dnf check-update
