7d44d54e17169eae2896d349faa89562ce153775
harthurd <55783388+harthurd@users.noreply.github.com>
2024-05-29 00:48:49 +0300

    Update system-postinstall.sh

--- system-postinstall.sh
+++ system-postinstall.sh
@@ -2,17 +2,17 @@
 
 # Install software
 dnf update -y
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-rpm --import https://www.virtualbox.org/download/oracle_vbox_2016.asc
-rpm --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-wget -O /etc/yum.repos.d/virtualbox.repo https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo
-dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+rpm --import https://packages.microsoft.com/keys/microsoft.asc \
+ https://www.virtualbox.org/download/oracle_vbox_2016.asc \
+ https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
+dnf config-manager --add-repo https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo \
+ https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
 dnf check-update
-dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf install -y https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
-dnf install -y mc htop pass alacritty wireguard-tools code VirtualBox sublime-text vim obs-studio vlc aria2
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+ https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+ https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm \
+ mc htop pass alacritty wireguard-tools code VirtualBox sublime-text vim obs-studio vlc aria2
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub md.obsidian.Obsidian com.skype.Client com.discordapp.Discord
 mkdir -p /tmp/distrib
